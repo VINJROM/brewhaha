4ca67234b4e94bc5ff5364a463cc451e7d48c973
Vincent Romine <vinjrom@live.com>
2020-08-11 15:55:19 -0500

    register user with Strapi

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,31 +1,66 @@
 import React from "react";
-import { Container, Box, Heading, Text, TextField, Button } from "gestalt";
+import { Container, Box, Button, Heading, Text, TextField } from "gestalt";
+import { setToken } from "./utils";
+import ToastMessage from "./ToastMessage";
+import Strapi from "strapi-sdk-javascript/build/main";
+const apiUrl = process.env.API_URL || "http://localhost:1337";
+const strapi = new Strapi(apiUrl);
 
 class Signup extends React.Component {
   state = {
     username: "",
     email: "",
     password: "",
+    toast: false,
+    toastMessage: "",
+    loading: false
   };
 
-  
   handleChange = ({ event, value }) => {
     event.persist();
     this.setState({ [event.target.name]: value });
   };
 
-  handleSubmit = (event) => {
+  handleSubmit = async event => {
     event.preventDefault();
-    if (!this.isFormEmpty(this.state)) {
-      console.log("submitted");
+    const { username, email, password } = this.state;
+
+    if (this.isFormEmpty(this.state)) {
+      this.showToast("Fill in all fields");
+      return;
     }
+    // Sign up user
+    try {
+      // Set loading - true
+      this.setState({loading: true})
+      // Make request to register user with Strapi
+      const response = await strapi.register(username, email, password);
+      // Set loading false
+      this.setState({loading: false})
+      // Put token to manage user session in local storage
+      setToken(response.jwt)
+      // Redirect user to home page  
+      this.redirectUser("/")
+    } catch (err) {
+      this.setState({ loading: false });
+      this.showToast(err.message);
+    }  
   };
+
+  redirectUser = path => this.props.history.push(path);
 
   isFormEmpty = ({ username, email, password }) => {
     return !username || !email || !password;
   };
 
+  showToast = (toastMessage) => {
+    this.setState({ toast: true, toastMessage });
+    setTimeout(() => this.setState({ toast: false, toastMessage: "" }), 5000);
+  };
+
   render() {
+    const { toastMessage, toast, loading } = this.state;
+
     return (
       <Container>
         <Box
@@ -56,7 +91,7 @@ class Signup extends React.Component {
               direction="column"
               alignItems="center"
             >
-              <Heading color="midnight"> Let's Get Started</Heading>
+              <Heading color="midnight">Let's Get Started</Heading>
               <Text italic color="orchid">
                 Sign up to order some brews!
               </Text>
@@ -69,7 +104,7 @@ class Signup extends React.Component {
               placeholder="Username"
               onChange={this.handleChange}
             />
-            {/* Email Input */}
+            {/* Email Address Input */}
             <TextField
               id="email"
               type="email"
@@ -85,9 +120,16 @@ class Signup extends React.Component {
               placeholder="Password"
               onChange={this.handleChange}
             />
-            <Button inline color="blue" text="Submit" type="submit" />
+            <Button
+              inline
+              disabled={loading}
+              color="blue"
+              text="Submit"
+              type="submit"
+            />
           </form>
         </Box>
+        <ToastMessage show={toast} message={toastMessage} />
       </Container>
     );
   }
